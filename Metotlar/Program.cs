a16fe9cdb9fbbec46ff666a6ea14d4646a82fdaa
batuhan6 <32600613+batuhan6@users.noreply.github.com>
2021-05-07 01:40:22 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string urunAdi2 = "Kavun";
+            double fiyatı2 = 15;
+            string aciklama2 = "Amasya elması";
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 1;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------------");
+            }
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.ekle(urun2);
+            sepetManager.ekle(urun1);
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi" + urun.Adi); 
+        }
+    }
+}
